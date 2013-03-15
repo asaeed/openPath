21ba07810a5b2c4e0bd91c9537e7f7f088c3edf3
Ahmad Saeed <asaeed@gmail.com>
2013-03-15 17:08:24 -0400

    persona tweaking

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.configure('development', function(){
 // });
 
 require("express-persona")(app, {
-  audience: "http://ec2-23-20-219-99.compute-1.amazonaws.com:8080/users",
+  audience: "http://ec2-23-20-219-99.compute-1.amazonaws.com:8080",
 });
 
 app.get('/', routes.index);
