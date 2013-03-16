31c64ff0f2b476462c0cf437647c3df0e293efe5
Ahmad Saeed <asaeed@gmail.com>
2013-03-16 00:40:47 -0400

    persona tweaking

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'openPath', email: /*req.session.email ||*/ null });
+  res.render('index', { title: 'openPath', email: req.session.email || null });
 };
