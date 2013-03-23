8cb2bec09f6331114529d5ca752cc4d36423f020
Ahmad Saeed <asaeed@gmail.com>
2013-03-23 16:57:42 -0400

    guest login option

--- app.js
+++ app.js
@@ -52,11 +52,11 @@ function requireLogin(req, res, next) {
   auth.status(req, res, function(){
     //if (req.session.email == "asaeed@gmail.com") {
     console.log("checking auth - user email: " + JSON.stringify(res.json));
-    if (res.email && res.email != "") {
+    //if (res.email && res.email != "") {
       next();
-    } else {
-      res.redirect("/");
-    }
+    //} else {
+    //  res.redirect("/");
+    //}
   });
 
 }
