4258717f8eccf082114f028ade25f1b7b15de959
Ahmad Saeed <asaeed@gmail.com>
2013-03-23 16:49:08 -0400

    guest login option

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.configure('development', function(){
 function requireLogin(req, res, next) {
   auth.status(req, res, function(){
     //if (req.session.email == "asaeed@gmail.com") {
-    console.log("checking auth - user email: " + JSON.stringify(res.json));
+    //console.log("checking auth - user email: " + JSON.stringify(res.json));
     if (res.email && res.email != "") {
       next();
     } else {
