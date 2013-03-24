a3c09e95f7763635e116be0d191f34f0f3ba66d9
Ahmad Saeed <asaeed@gmail.com>
2013-03-23 23:58:02 -0400

    guest login option

--- routes/auth.js
+++ routes/auth.js
@@ -11,26 +11,33 @@ exports.status = function authStatus(req, res) {
     }
   }, function(e, r, body) {
     if(body && body.email) {
-      // if new user, then create them in database
-      user.findByEmail(body.email, function(foundUser){
-        console.log(foundUser);
-        // user.addGuestUser("guest", function(newGuest){
-        //   req.session.email = newGuest.email;
-        //   req.session.userId = newGuest._id;
-        //   newGuest.status = "okay";
-        //   res.json(newGuest);
-        // });
-      });
-        
-      req.session.email = body.email;
-      res.json(body);
       console.log("auth status: persona");
+
+      // find user, if new user, then create them in database
+      user.findByEmail(body.email, function(foundUser){
+        if (!foundUser) {
+          user.addUser(body.email, function(newUser){
+            req.session.email = newUser.email;
+            req.session.userId = newUser._id;
+            newUser.status = "okay";
+            res.json(newUser);
+          });
+        } else {
+          req.session.email = foundUser.email;
+          req.session.userId = foundUser._id;
+          foundUser.status = "okay";
+          res.json(foundUser);
+        }
+      });    
+      
     } else {
+
       // if not authenticated, is user a guest?
       if (req.session.email == "guest"){
         console.log("auth status: guest");
         res.json({"email": "Guest1234", "status": "okay"});
       }
+
     }
   });
 };
@@ -42,7 +49,7 @@ exports.logout = function authLogout(req, res) {
 
 exports.guest = function authGuest(req, res) {
   // create new guest user
-  user.addGuestUser("guest", function(newGuest){
+  user.addUser("guest", function(newGuest){
     req.session.email = newGuest.email;
     req.session.userId = newGuest._id;
     newGuest.status = "okay";
