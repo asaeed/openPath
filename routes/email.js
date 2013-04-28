cdc55596b6be99bce1de1cacd7e48a85748171e4
Ahmad Saeed <asaeed@gmail.com>
2013-04-27 22:02:22 -0400

    adding email utility module

---
+++ routes/email.js
@@ -0,0 +1,36 @@
+
+
+var config = require('../config');
+var email  = require("emailjs/email");
+
+var server = email.server.connect({
+   user:     config.user, 
+   password: config.password, 
+   host:     config.host, 
+   ssl:      true
+});
+
+var message = {
+   text:    "i hope this works", 
+   from:    "you <asaeed@gmail.com>", 
+   to:      "someone <asaeed@gmail.com>, another <asaeed@gmail.com>",
+   cc:      "",
+   subject: "testing emailjs"
+};
+
+server.send(message, function(err, message) { console.log(err || message); });
+
+
+exports.findByEmail = function(email, callback) {
+    console.log('about to send an email...');
+
+	var message = {
+	   text:    "i hope this works", 
+	   from:    "you <asaeed@gmail.com>", 
+	   to:      "someone <asaeed@gmail.com>, another <asaeed@gmail.com>",
+	   cc:      "",
+	   subject: "testing emailjs"
+	};
+
+	server.send(message, function(err, message) { console.log(err || message); });
+};
