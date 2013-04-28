29f79199e8ae1d7ec2b9092c535d76f302ecdbec
Ahmad Saeed <asaeed@gmail.com>
2013-04-27 22:23:42 -0400

    adding email utility module

--- routes/email.js
+++ routes/email.js
@@ -10,22 +10,7 @@ var server = email.server.connect({
    ssl:      true
 });
 
-console.log(config.email.user);
-console.log(config.email.password);
-console.log(config.email.host);
-
-var message = {
-   text:    "i hope this works", 
-   from:    "you <asaeed@gmail.com>", 
-   to:      "someone <asaeed@gmail.com>, another <asaeed@gmail.com>",
-   cc:      "",
-   subject: "testing emailjs"
-};
-
-server.send(message, function(err, message) { console.log(err || message); });
-
-
-exports.findByEmail = function(email, callback) {
+exports.sendEmail = function(toEmail, callback) {
     console.log('about to send an email...');
 
 	var message = {
