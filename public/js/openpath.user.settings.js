70568a4d390f7c8def0fcc7b8c18197941fa21f2
Jamie Gilmartin <jamie@jpg-macbook.(none)>
2013-08-02 08:39:17 -0400

    fix settings populating

--- public/js/openpath.user.settings.js
+++ public/js/openpath.user.settings.js
@@ -14,6 +14,11 @@ OpenPath.user.settings = {
 		//dom eles
 		this.settingsWrapper = $('#settings');
 		this.form = this.settingsWrapper.find('form');
+		this.alertsColearnerJoin = this.form.find('#alertsColearnerJoin');
+		this.alertsNearEvent = this.form.find('#alertsNearEvent');
+		this.alertsAllEvents = this.form.find('#alertsAllEvents');
+		this.profileAccessPublic = this.form.find('#profileAccessPublic');
+		this.profileAccessPrivate = this.form.find('#profileAccessPrivate');
 		
 		//actions
 		OpenPath.user.getByEmail(function(d){
@@ -22,9 +27,9 @@ OpenPath.user.settings = {
 		
 		this.form.submit(function(e){
 			//console.log('update profile',username,email,sessionID);
-			var alertsColearnerJoin = $(this).find('#alertsColearnerJoin').val(),
-				alertsNearEvent = $(this).find('#alertsNearEvent').val(),
-				alertsAllEvents = $(this).find('#alertsAllEvents').val(),
+			var alertsColearnerJoin = self.alertsColearnerJoin.is(':checked'),
+				alertsNearEvent = self.alertsNearEvent.is(':checked'),
+				alertsAllEvents = self.alertsAllEvents.is(':checked'),
 				profileAccess = $(this).find('input:radio[name=profileaccess]:checked').val(),
 				data = {
 					'settings' : {
@@ -46,7 +51,23 @@ OpenPath.user.settings = {
 	},
 	populate : function( data ){
 		console.log('populate',data.email,data._id);
+		console.log(data.settings);
 		//alertsColearnerJoin,alertsNearEvent,alertsAllEvents,profileAccess
-
+		if(data.settings.alertsColearnerJoin === 'true'){
+			this.alertsColearnerJoin.attr('checked','checked');
+		}
+		if(data.settings.alertsNearEvent === 'true'){
+			this.alertsNearEvent.attr('checked','checked');
+		}
+		if(data.settings.alertsAllEvents === 'true'){
+			this.alertsAllEvents.attr('checked','checked');
+		}
+		if(data.settings.profileAccess === 'private'){
+			this.profileAccessPrivate.attr('checked','checked');
+		}else{
+			this.profileAccessPublic.attr('checked','checked');
+		}
+		
+		
 	}
 };
