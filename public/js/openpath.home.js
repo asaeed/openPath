43785f1c7c024e4ac5b11316aa0465eaa04f8f99
Jamie Gilmartin <jamiegilmartin@gmail.com>
2013-09-05 23:42:25 -0400

    remove old guest login btn clicks

--- public/js/openpath.home.js
+++ public/js/openpath.home.js
@@ -2,14 +2,10 @@ OpenPath = window.OpenPath || {};
 
 
 OpenPath.home = {
-	init : function(){
-
-		//TODO: clean below and add to name space
-		console.log('openPath.home.init',this) 
-				
-		this.loginBtns();
+	init : function(){				
+		this.events();
 	},
-	loginBtns : function(){
+	events : function(){
 		/**
 		 * TODO: determine if we are adding guest back in
 		 */
@@ -17,32 +13,7 @@ OpenPath.home = {
 		$('#loginbtn').mouseup(function() {
 			navigator.id.request();
 		});
-		// display guest login form
-		$('#guestbtn').mouseup(function() {
-			$("#login").fadeOut();
-			$("#guestlogin").fadeIn();
-		});
-		
-		// validate email and show main
-		$('#guestloginbtn').mouseup(function() {
-			var email = $('#email').val();
-			var isValid = validateEmail(email);
-			if(!isValid){
-				$('#emailerror').modal();
-			}else{
-				// send email to API to authenticate
-				var xhr = new XMLHttpRequest();
-				xhr.open("GET", "/auth/guest", true);
-				xhr.addEventListener("loadend", function(e) {
-					var data = JSON.parse(this.responseText);
-					if (data && data.status === "okay") {
-						console.log('going to main');
-						window.location = "/main";
-					}
-				}, false);
-				xhr.send();
-			};
-		});
+
 	}
 
 };
