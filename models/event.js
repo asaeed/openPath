de69834e520c68e457e04d666fbb00bd2e443886
jamiegilmartin <jamiegilmartin@gmail.com>
2014-03-23 17:04:33 -0400

    page organization, more events

--- models/event.js
+++ models/event.js
@@ -9,17 +9,25 @@ EventSchema = mongoose.Schema({
 	name: String,
 	link: String,
 	description: String,
-	location: {}, 
+	location: {
+		lat : Number,
+		lng : Number
+	}, 
 	grades: [String],
 	startDate :  Date,
 	endDate : Date
 });
 
 EventSchema.statics.addEvent = function(req, done){
+	console.log('ev req = ', req.body)
 	this.create({
 		creatorID : req.user._id,
 		dateCreated : Date.now(),
-		dateModified : Date.now()
+		dateModified : Date.now(),
+		name: req.body.name,
+		link: req.body.link,
+		description: req.body.description
+		//dates, grade, location
 	}, function(err, newEvent){
 		if(err) throw err;
 		// if (err) return done(err);
--- public/js/main.js
+++ public/js/main.js
@@ -22,47 +22,10 @@ OpenPath = {
 		//count();
 
 
-		//dom vars
-		var intro = document.getElementById('intro'),
-			home = document.getElementById('home');
-
-
-		if( home ){
-			this.Router.init();
-			this.Router.checkRoute();
-			
-			this.peerHandler();
-
-
-		}else{
-			this.intro();
-		}
-	},
-	/**
-	 * intro page toggle signup, login, callout...
-	 */
-	intro : function(){
-		var toggles = document.getElementsByClassName('toggle'),
-			signupBtn = document.getElementById('signupBtn'),
-			loginBtn = document.getElementById('loginBtn');
-
-		function toggle( className ){
-			for(var i=0;i<toggles.length;i++){
-				if(toggles[i].classList.contains(className)){
-					toggles[i].style.display = 'block';
-				}else{
-					toggles[i].style.display = 'none';
-				}
-				
-			}
-		}
-		//events
-		loginBtn.addEventListener('click',function(){
-			toggle('login');
-		},false);
-		signupBtn.addEventListener('click',function(){
-			toggle('signup');
-		},false);
+		this.Router.init();
+		this.Router.checkRoute();
+		
+		this.peerHandler();		
 	},
 	peerHandler : function(){
 		var self = this;
